0d916d22e3d8ce29455ed31f7ae3d35a461e4e09
JHarithaR <haritharani92@gmail.com>
2024-11-12 14:19:43 +0530

    api context handled

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { ApiContext } from './contextfile/apiContext';
+import UserTable from './userTable';
+
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleMode =() =>{
+    setDarkMode(!darkMode);
+    if(!darkMode)
+    {
+      document.documentElement.classList.add('dark');
+    }
+    else
+    {
+      document.documentElement.classList.remove('dark');
+
+    }
+  }
   return (
-    <>
-    <div className='text-3xl'>
-    welcome
-    </div> 
-  </>
+   <ApiContext>
+    <button onClick = {toggleMode}>{darkMode ?'Light Mode':'Dark Mode'}</button>
+    <h1 className='text-center text-3xl text-black mb-4 dark:text-white '> User Management Dashboard</h1> 
+    <UserTable/>
+    </ApiContext>
   )
 }
 
---
+++ src/contextfile/apiContext.tsx
@@ -0,0 +1,89 @@
+import { createContext,ReactNode,useContext, useEffect, useState } from "react";
+
+interface Geo {
+        lat: string;
+        lng: string;
+}
+
+interface Address
+{
+        street: string;
+        suite: string;
+        city: string;
+        zipcode: string;
+        geo: Geo;
+}
+
+interface Company{
+        name: string;
+        catchPhrase: string;
+        bs: string;
+}
+
+
+interface UserDetails {
+  id: number;
+  name: string;
+  email: string;
+  address:Address;
+  phone: string;
+  website: string;
+  company: Company;
+}
+
+interface userContextType
+{
+    data:UserDetails[];
+    load:boolean;
+    error: string |null;
+}
+
+const APIdata= createContext<userContextType |undefined>(undefined);
+
+export const ApiContext: React.FC<{children:ReactNode}>= ({children}) => {
+  const [data, setData] = useState<UserDetails[]>([]);
+  const [load, setLoad] = useState<boolean>(false);
+  const[error,setError] = useState<string|null>(null);
+
+  const url: string = "https://jsonplaceholder.typicode.com/users";
+  useEffect(() => {
+    const fetchUsers = async () =>{
+        try{
+            const response = await fetch(url);
+            if(!response.ok)
+            {
+                throw new Error('failed')
+            }
+            const data: UserDetails[] = await response.json();
+            setData(data);
+            setLoad(true)
+        }
+        catch(err:any)
+        {
+           setError(err.message) 
+        }
+        finally
+        {
+            setLoad(false)
+        }
+    };
+    fetchUsers();
+  
+  }, []);
+  console.log(data, load,error);
+  return (
+    <APIdata.Provider value={{data,load, error}}>
+    {children}
+    </APIdata.Provider>
+  );
+};
+
+
+export const useUsers = ():userContextType => {
+const contextData = useContext(APIdata);
+if(contextData === undefined)
+{
+    throw new Error("CONTEXT DATA UNDEFINED")
+}
+return contextData;
+}
